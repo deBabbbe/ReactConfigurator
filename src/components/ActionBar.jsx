bb71da4beaf01a3d28637d35e499e2f7507d7888
deBabbbe <neotbh@freenet.de>
2019-09-24 22:43:28 +0200

    Added refresh button to actionbar

--- src/components/ActionBar.jsx
+++ src/components/ActionBar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSave, faPlusSquare } from '@fortawesome/free-solid-svg-icons'
+import { faSave, faPlusSquare, faRedoAlt } from '@fortawesome/free-solid-svg-icons'
 
 export function ActionBar(props) {
     return <div className="ActionBar">
         Configurator
+        <FontAwesomeIcon id="refreshButton" icon={faRedoAlt} size="lg" title="Neu laden"/>
         <FontAwesomeIcon id="saveButton" icon={faSave} size="lg" title="Speichern" onClick={props.save} />
         <FontAwesomeIcon id="addButton" icon={faPlusSquare} size="lg" title="Hinzufügen" onClick={props.addEntry} />
     </div>;
